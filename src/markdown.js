7384ac46fa65897d741f257de5f72d5132d88dbb
Asbjørn Enge <asbjorn@hanafjedle.net>
2023-08-28 16:22:43 +0200

    Cleanup

--- src/markdown.js
+++ src/markdown.js
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import SimpleMDE from "react-simplemde-editor"
 import "easymde/dist/easymde.min.css"
 
-export default function MarkdownEditor({}) {
+export default function MarkdownEditor() {
   // eslint-disable-next-line
   const [markdown, setMarkdown] = useState('# Hello')
 
   return (
     <SimpleMDE value={markdown} onChange={setMarkdown} />
   )
-} 
+}
