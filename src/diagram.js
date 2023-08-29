1ba2dcaa40ae7db979e57b2bf2a43188e12d82fc
Asbjørn Enge <asbjorn@hanafjedle.net>
2023-08-29 10:33:07 +0200

    Basic node menu

--- src/diagram.js
+++ src/diagram.js
@@ -1,4 +1,4 @@
-import { useCallback } from 'react'
+import { useState, useRef, useCallback } from 'react'
 import ReactFlow, {
   MiniMap,
   Controls,
@@ -7,6 +7,7 @@ import ReactFlow, {
   useEdgesState,
   addEdge,
 } from 'reactflow'
+import ContextMenu from './nodes/ContextMenu' 
 import 'reactflow/dist/style.css'
 
 const initialNodes = [
@@ -20,20 +21,47 @@ export default function DiagramEditor() {
   // eslint-disable-next-line
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+  const [menu, setMenu] = useState(null)
+  const ref = useRef(null)
 
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges])
 
+  const onNodeContextMenu = useCallback(
+    (event, node) => {
+      // Prevent native context menu from showing
+      event.preventDefault();
+
+      // Calculate position of the context menu. We want to make sure it
+      // doesn't get positioned off-screen.
+      const pane = ref.current.getBoundingClientRect();
+      setMenu({
+        id: node.id,
+        top: event.clientY < pane.height - 200 && event.clientY,
+        left: event.clientX < pane.width - 200 && event.clientX,
+        right: event.clientX >= pane.width - 200 && pane.width - event.clientX,
+        bottom: event.clientY >= pane.height - 200 && pane.height - event.clientY,
+      })
+    },
+    [setMenu]
+  )
+
+  const onPaneClick = useCallback(() => setMenu(null), [setMenu])
+
   return (
     <ReactFlow
+      ref={ref}
       nodes={nodes}
       edges={edges}
+      onConnect={onConnect}
+      onPaneClick={onPaneClick}
       onNodesChange={onNodesChange}
       onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
+      onNodeContextMenu={onNodeContextMenu}
     >
       <MiniMap />
       <Controls />
       <Background />
+      {menu && <ContextMenu onClick={onPaneClick} {...menu} />}
     </ReactFlow>
   )
 }
