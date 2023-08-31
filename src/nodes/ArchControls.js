ae337fd9602d1cc98909a6c277c4195a37038907
Asbjørn Enge <asbjorn@hanafjedle.net>
2023-08-31 10:07:51 +0200

    Icons

--- src/nodes/ArchControls.js
+++ src/nodes/ArchControls.js
@@ -1,9 +1,11 @@
 import {
   Controls,
-  ControlButton,
-  useStore
+  ControlButton
 } from 'reactflow'
+import styled from 'styled-components'
 import { create } from 'zustand'
+import { FiArrowUpRight } from 'react-icons/fi'
+import { AiOutlineAppstoreAdd } from 'react-icons/ai'
 
 export const controlState = create((set) => ({
   adding: false,
@@ -13,14 +15,37 @@ export const controlState = create((set) => ({
 export default function ArchControls() {
   const { adding, toggleAddingEdge } = controlState()
 
+  const edgeAddingStyle = {}
+  if (adding) edgeAddingStyle.backgroundColor = 'var(--color-bg-dark-grey)' 
+
   return (
     <Controls>
-      <ControlButton onClick={toggleAddingEdge} title="another action">
-        <div>E</div>
+      <ControlButton style={edgeAddingStyle} onClick={toggleAddingEdge} title="another action">
+        <ControlIconContainer>
+          <FiArrowUpRight />
+        </ControlIconContainer>
       </ControlButton>
       <ControlButton onClick={() => console.log('action')} title="action">
-        <div>B</div>
+        <ControlIconContainer>
+          <AiOutlineAppstoreAdd />
+        </ControlIconContainer>
       </ControlButton>
     </Controls>
   );
 }
+
+const ControlIconContainer = styled.div`
+  width: 20px;
+  height: 20px;
+  flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & svg {
+    width: 15px;
+    height: 15px;
+    max-width: 20px;
+    max-height: 20px;
+    display: flex;
+  }
+`
