bc1fce9d844b223459d0a4e8f2f91268fc823bad
Asbjørn Enge <asbjorn@hanafjedle.net>
2023-09-14 14:25:59 +0200

    Export to png

--- src/nodes/ArchControls.js
+++ src/nodes/ArchControls.js
@@ -6,14 +6,16 @@ import {
 } from 'reactflow'
 import { uid } from 'uid'
 import { FiArrowUpRight } from 'react-icons/fi'
-import { AiOutlineAppstoreAdd } from 'react-icons/ai'
+import { AiOutlineExport, AiOutlineAppstoreAdd } from 'react-icons/ai'
+import { toPng } from 'html-to-image'
+import { getRectOfNodes, getTransformForBounds } from 'reactflow'
 import { SVGIconContainer } from '../components/SVGIconContainer'
 import { controlState } from '../state'
 
 export default function ArchControls({ setNodes }) {
   // TODO: Sett adding mode while holding down ctrl ??
   const { adding, toggleAddingEdge, setAddingEdge } = controlState()
-  const { project } = useReactFlow()
+  const { project, getNodes } = useReactFlow()
 
   useEffect(() => {
     function downHandler({key}) {
@@ -51,14 +53,45 @@ export default function ArchControls({ setNodes }) {
     setNodes((nds) => nds.concat(newNode))
   }
 
+  const handleSaveDiagram = async () => {
+    // we calculate a transform for the nodes so that all nodes are visible
+    // we then overwrite the transform of the `.react-flow__viewport` element
+    // with the style option of the html-to-image library
+    const viewport = document.querySelector('.react-flow__viewport')
+    const viewportBounds = viewport.getBoundingClientRect()
+    const imageWidth = viewportBounds.width * 2 // TODO: Allow modify resolution ?
+    const imageHeight = viewportBounds.height * 2
+    const nodesBounds = getRectOfNodes(getNodes())
+    const transform = getTransformForBounds(nodesBounds, imageWidth, imageHeight, 0.5, 2)
+    const dataUrl = await toPng(document.querySelector('.react-flow__viewport'), {
+      backgroundColor: 'white',
+      width: imageWidth,
+      height: imageHeight,
+      style: {
+        width: imageWidth,
+        height: imageHeight,
+        transform: `translate(${transform[0]}px, ${transform[1]}px) scale(${transform[2]})`,
+      },
+    })
+    const a = document.createElement('a');
+    a.setAttribute('download', 'arch.png');
+    a.setAttribute('href', dataUrl);
+    a.click();
+  }
+
   return (
     <Controls>
-      <ControlButton style={edgeAddingStyle} onClick={toggleAddingEdge} title="another action">
+      <ControlButton onClick={handleSaveDiagram} title="save">
+        <SVGIconContainer iconsize={15}>
+          <AiOutlineExport />
+        </SVGIconContainer>
+      </ControlButton>
+      <ControlButton style={edgeAddingStyle} onClick={toggleAddingEdge} title="edges">
         <SVGIconContainer iconsize={15}>
           <FiArrowUpRight />
         </SVGIconContainer>
       </ControlButton>
-      <ControlButton onClick={handleAddNode} title="action">
+      <ControlButton onClick={handleAddNode} title="new node">
         <SVGIconContainer iconsize={15}>
           <AiOutlineAppstoreAdd />
         </SVGIconContainer>
